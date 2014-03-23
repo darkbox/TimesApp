c0f2bbf032e768bb5d1ab03b5e66f623dcb3d831
darkbox <rgtresd@gmail.com>
2014-03-23 17:44:37 +0100

    Security salt y seed configurado

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'fc7f678b2389480bca36411d0e25442890df5e9f');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '768937458927345234523400981245');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
