d6a716f156707f424b7f45db4de2773119b9490c
darkbox <rgtresd@gmail.com>
2014-04-01 18:57:03 +0200

    Cambiado el campo status
    El campo status has pasado de INT a ENUM

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -31,16 +31,6 @@ class Project extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'status' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'client_id' => array(
 			'numeric' => array(
 				'rule' => array('numeric'),
