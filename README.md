ac2eee5c54cb3b45089c0140d9da8a2edc4a7303
darkbox <rgtresd@gmail.com>
2014-04-01 18:07:59 +0200

    Configurado el fichero de la base de datos y fichero README actualizado

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ TimesApp
 ========
 
 A free and open source web application for tracking your time while working.
+
+##Database info:
++ __server:__ localhost
++ __username:__ timesapp
++ __password:__ timesapp
--- app/Config/database.php
+++ app/Config/database.php
@@ -65,21 +65,10 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
+		'login' => 'timesapp',
+		'password' => 'timesapp',
+		'database' => 'timesapp',
 		'prefix' => '',
-		//'encoding' => 'utf8',
-	);
-
-	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
-		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 }
