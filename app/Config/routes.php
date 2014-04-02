e890899603cdab96269b004a078d5b3b962a6989
darkbox <rgtresd@gmail.com>
2014-04-02 19:27:14 +0200

    Redirección por defecto al Dashboard

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -24,7 +24,8 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'dashboard', 'action' => 'index'));
+	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
