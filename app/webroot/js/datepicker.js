6243ec9dedc484f3267408d2d3afab0aac65145b
souanyirer <souanyirer@gmail.com>
2014-04-23 23:18:26 +0200

    mejorada posición de datepicker

--- app/webroot/js/datepicker.js
+++ app/webroot/js/datepicker.js
@@ -3,22 +3,30 @@ $('#dpProjectDeadLine').prop('disabled', true);
 $('#dpProjectStartingDate').Zebra_DatePicker({
   direction: true,
   pair: $('#dpProjectDeadLine'),
+  show_icon: false,
+  offset: [-300, -5],
 
-  onChange: function() {
+  onSelect: function() {
   	$('#dpProjectDeadLine').prop('disabled', false);
   	$('#dpProjectDeadLine + button').removeClass("Zebra_DatePicker_Icon_Disabled");
   }
 });
 
 $('#dpProjectDeadLine').Zebra_DatePicker({
-  direction: 1
+  direction: 1,
+  show_icon: false,
+  offset: [-300, -5]
 });
 
 $('#dpProjectStartingDateEdit').Zebra_DatePicker({
   direction: true,
+  show_icon: false,
+  offset: [-300, -5],
   pair: $('#dpProjectDeadLineEdit')
 });
 
 $('#dpProjectDeadLineEdit').Zebra_DatePicker({
-  direction: 1
+  direction: 1,
+  offset: [-300, -5],
+  show_icon: false
 });
