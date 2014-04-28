34a87c0ef2919f1eb9b58ee590a0311f1c1b11d4
darkbox <rgtresd@gmail.com>
2014-04-28 20:26:57 +0200

    BD v0.1.4

--- timesapp_v0.1.4.sql
+++ timesapp_v0.1.4.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 27, 2014 at 11:15 AM
+-- Generation Time: Apr 28, 2014 at 06:26 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
 
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `hours` (
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
 
 --
 -- Dumping data for table `hours`
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `hours` (
 INSERT INTO `hours` (`id`, `project_id`, `service_id`, `user_id`, `hours`, `billed`, `note`, `created`, `modified`) VALUES
 (2, 4, 1, 1, '5.30', 0, 'this is a test note.', '2014-04-13 15:01:20', '2014-04-13 15:01:20'),
 (3, 4, 1, 1, '3.00', 0, 'This is another test note.', '2014-04-13 15:03:15', '2014-04-13 15:03:15'),
-(4, 4, 1, 1, '3.00', 0, 'This is another test note.', '2014-04-13 15:04:01', '2014-04-13 15:04:01'),
+(4, 4, 1, 1, '3.00', 1, 'This is another test note.', '2014-04-13 15:04:01', '2014-04-13 15:04:01'),
 (5, 4, 1, 1, '2.00', 0, 'The test note strikes again.', '2014-04-13 15:05:45', '2014-04-13 15:05:45'),
 (6, 4, 1, 1, '1.23', 0, 'I hope this will be the last test note cause it''s driving me crazy.', '2014-04-13 15:09:15', '2014-04-13 15:09:15'),
 (7, 2, 1, 1, '11.56', 1, 'Doing some stuff.', '2014-04-13 15:19:13', '2014-04-13 15:19:13'),
@@ -94,7 +94,8 @@ INSERT INTO `hours` (`id`, `project_id`, `service_id`, `user_id`, `hours`, `bill
 (11, 1, 1, 1, '60.00', 1, 'Comprobando las horas', '2014-04-16 12:32:02', '2014-04-16 12:32:02'),
 (14, 1, 1, 1, '0.02', 1, 'Comprobando el timer.', '2014-04-17 11:51:40', '2014-04-17 11:51:40'),
 (15, 1, 1, 1, '1.67', 1, 'Continuo con las pruebas en el timer.', '2014-04-17 11:59:18', '2014-04-17 11:59:18'),
-(17, 1, 1, 1, '0.02', 1, 'Comprobado, el timer funciona correctamente.', '2014-04-17 12:27:17', '2014-04-17 12:27:17');
+(17, 1, 1, 1, '0.02', 1, 'Comprobado, el timer funciona correctamente.', '2014-04-17 12:27:17', '2014-04-17 12:27:17'),
+(18, 1, 3, 1, '23.00', 1, 'Mucho trabajo, poco dinero.', '2014-04-27 12:09:38', '2014-04-27 12:09:38');
 
 -- --------------------------------------------------------
 
@@ -109,6 +110,7 @@ CREATE TABLE IF NOT EXISTS `invoices` (
   `title` varchar(60) NOT NULL,
   `status` int(2) NOT NULL,
   `due` int(11) NOT NULL,
+  `amount` decimal(10,0) NOT NULL DEFAULT '0',
   `currency_symbol` varchar(5) DEFAULT NULL,
   `currency_code` varchar(3) DEFAULT NULL,
   `discount` double DEFAULT '0',
@@ -120,16 +122,18 @@ CREATE TABLE IF NOT EXISTS `invoices` (
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `invoices`
 --
 
-INSERT INTO `invoices` (`id`, `project_id`, `client_id`, `title`, `status`, `due`, `currency_symbol`, `currency_code`, `discount`, `terms`, `note`, `invoice_date`, `due_date`, `display_country`, `created`, `modified`) VALUES
-(1, 3, 0, 'Whatever', 3, 500, '', '', 0, '', '', NULL, NULL, 0, '2014-04-20 18:12:38', '2014-04-20 18:13:50'),
-(2, 3, 0, 'Whatever', 4, 500, '', '', 0, '', '', NULL, NULL, 0, '2014-04-20 18:12:38', '2014-04-23 16:00:45'),
-(8, NULL, 3, 'Probando facturas', 0, 0, '$', 'USD', 0, 'No hay términos.', 'Esto es una prueba.', '2014-04-27', '2014-04-30', 1, '2014-04-27 10:42:45', '2014-04-27 10:42:45');
+INSERT INTO `invoices` (`id`, `project_id`, `client_id`, `title`, `status`, `due`, `amount`, `currency_symbol`, `currency_code`, `discount`, `terms`, `note`, `invoice_date`, `due_date`, `display_country`, `created`, `modified`) VALUES
+(1, 3, 0, 'Whatever', 3, 500, '0', '', '', 0, '', '', NULL, NULL, 0, '2014-04-20 18:12:38', '2014-04-20 18:13:50'),
+(2, 3, 0, 'Whatever', 4, 500, '0', '', '', 0, '', '', NULL, NULL, 0, '2014-04-20 18:12:38', '2014-04-23 16:00:45'),
+(8, NULL, 3, 'Probando facturas', 0, 0, '0', '$', 'USD', 0, 'No hay términos.', 'Esto es una prueba.', '2014-04-27', '2014-04-30', 1, '2014-04-27 10:42:45', '2014-04-27 10:42:45'),
+(9, NULL, 1, 'Comprobando ', 0, 0, '0', '€', 'EUR', 0, 'nada de nada', 'Sin nota', '2014-04-27', '2014-04-30', 1, '2014-04-27 13:35:50', '2014-04-27 13:35:50'),
+(11, NULL, 3, 'Test invoice', 0, 0, '489', '€', 'EUR', 0, 'Please, pay me, I''m begging you!', 'Thank you!', '2014-04-28', '2014-04-30', 1, '2014-04-28 18:23:47', '2014-04-28 18:23:47');
 
 -- --------------------------------------------------------
 
@@ -149,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `lines` (
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
 -- Dumping data for table `lines`
@@ -159,7 +163,20 @@ INSERT INTO `lines` (`id`, `invoice_id`, `tax_id`, `type`, `code`, `description`
 (8, 8, 6, 1, 'Mantenimiento del sistema', 'Mantenimiento integral del sistema TimesApp', 10, 40, '2014-04-27 10:42:45', '2014-04-27 10:42:45'),
 (9, 8, 6, 1, 'Standard modeling', 'A standard modeling hour.', 10, 30, '2014-04-27 10:42:45', '2014-04-27 10:42:45'),
 (10, 8, 5, 1, 'Texturing', 'Texturing proccess', 18, 25, '2014-04-27 10:42:45', '2014-04-27 10:42:45'),
-(11, 8, 6, 2, 'USB stick', 'Usb stick 1Gb storage', 15, 1, '2014-04-27 10:42:45', '2014-04-27 10:42:45');
+(11, 8, 6, 2, 'USB stick', 'Usb stick 1Gb storage', 15, 1, '2014-04-27 10:42:45', '2014-04-27 10:42:45'),
+(12, 9, 6, 1, 'Mantenimiento del sistema', 'Mantenimiento integral del sistema TimesApp', 10, 61.71, '2014-04-27 13:35:50', '2014-04-27 13:35:50'),
+(13, 9, 5, 1, 'Texturing', 'Texturing proccess', 18, 23, '2014-04-27 13:35:50', '2014-04-27 13:35:50'),
+(14, 9, 6, 1, 'Standard modeling', 'A standard modeling hour.', 10, 2, '2014-04-27 13:35:50', '2014-04-27 13:35:50'),
+(15, 9, 6, 2, 'USB stick', 'Usb stick 1Gb storage', 15, 0, '2014-04-27 13:35:50', '2014-04-27 13:35:50'),
+(16, 10, 6, 1, 'Standard modeling', 'A standard modeling hour.', 10, 5, '2014-04-28 18:19:39', '2014-04-28 18:19:39'),
+(17, 10, 6, 1, 'Standard modeling', 'A standard modeling hour.', 10, 18, '2014-04-28 18:19:39', '2014-04-28 18:19:39'),
+(18, 10, 6, 1, 'Standard modeling', 'A standard modeling hour.', 10, 0, '2014-04-28 18:19:39', '2014-04-28 18:19:39'),
+(19, 10, 6, 2, 'USB stick 0001', 'Usb stick 4Gb storage', 25, 1, '2014-04-28 18:19:39', '2014-04-28 18:19:39'),
+(20, 10, 6, 2, 'ASD8689577', 'Maintenance pack - Pro', 39.99, 1, '2014-04-28 18:19:39', '2014-04-28 18:19:39'),
+(21, 11, 6, 1, 'Mantenimiento del sistema', 'Mantenimiento integral del sistema TimesApp', 10, 10, '2014-04-28 18:23:47', '2014-04-28 18:23:47'),
+(22, 11, 6, 1, 'Standard modeling', 'A standard modeling hour.', 10, 20, '2014-04-28 18:23:47', '2014-04-28 18:23:47'),
+(23, 11, 5, 1, 'Texturing', 'Texturing proccess', 18, 5, '2014-04-28 18:23:47', '2014-04-28 18:23:47'),
+(24, 11, 6, 2, 'USB stick', 'Usb stick 1Gb storage', 15, 1, '2014-04-28 18:23:47', '2014-04-28 18:23:47');
 
 -- --------------------------------------------------------
 
@@ -233,11 +250,11 @@ CREATE TABLE IF NOT EXISTS `projects` (
 --
 
 INSERT INTO `projects` (`id`, `code`, `status`, `description`, `client_id`, `init_date`, `deadline`, `estimate_time`, `estimate_price`, `billable`, `created`, `modified`) VALUES
-(1, 'Mi primer proyecto', 0, 'Este proyecto es de prueba.', 1, NULL, NULL, '01:00:00', NULL, 1, '2014-04-09 19:09:47', '2014-04-09 19:09:47'),
+(1, 'Mi primer proyecto', 1, 'Este proyecto es de prueba.', 1, NULL, NULL, '01:00:00', NULL, 1, '2014-04-09 19:09:47', '2014-04-27 12:09:08'),
 (2, 'TimesApp', 1, 'Una aplicación para ayudar a controlar el tiempo de tus proyectos y facturar mejor.', 1, '2014-03-19', '2014-06-09', '00:00:00', 3000, 0, '2014-04-12 15:17:36', '2014-04-12 15:17:36'),
 (3, 'Blog projectTimesApp', 2, '', 1, NULL, NULL, '15:38:00', NULL, 0, '2014-04-12 15:18:55', '2014-04-12 15:39:03'),
 (4, 'Proyecto con Node js', 3, 'Proyecto realizado en Node js y MongoBD', 3, NULL, NULL, '15:39:00', NULL, 1, '2014-04-12 15:19:17', '2014-04-13 14:29:04'),
-(5, 'Encuestas & informes', 2, '', 3, NULL, NULL, NULL, 3000, 0, '2014-04-12 15:40:43', '2014-04-12 15:40:43');
+(5, 'Encuestas & informes', 2, '', 3, NULL, NULL, NULL, 3000, 1, '2014-04-12 15:40:43', '2014-04-27 12:21:28');
 
 -- --------------------------------------------------------
 
@@ -290,7 +307,7 @@ CREATE TABLE IF NOT EXISTS `taxes` (
 --
 
 INSERT INTO `taxes` (`id`, `description`, `status`, `rate`, `created`, `modified`) VALUES
-(5, 'IVA 20%', 0, 20, '2014-04-01 19:46:04', '2014-04-02 18:57:19'),
+(5, 'IVA 20%', 1, 20, '2014-04-01 19:46:04', '2014-04-28 16:46:13'),
 (6, 'IVA 21%', 1, 21, '2014-04-01 19:46:19', '2014-04-02 18:53:03'),
 (7, 'IVA 18%', 0, 18, '2014-04-02 19:15:45', '2014-04-02 19:15:45'),
 (8, 'IVA 4%', 0, 4, '2014-04-02 19:26:31', '2014-04-02 19:26:31'),
@@ -336,7 +353,7 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `status`, `creat
 (2, 'Jose', 'souanyirer@gmail.com', '4e05c61fa71454de69a7d59d1a43f92477169877', 'overlord', 1, '2014-04-05 14:15:39', '2014-04-20 11:12:31'),
 (3, 'Paco', 'pacovalmisa@gmail.com', '27a40e13da83533c08c1524b6a4787a8e11143f3', 'minion', 1, '2014-04-05 15:26:31', '2014-04-10 15:59:29'),
 (4, 'Jimmy', 'jimmy@timesapp.com', 'b2000b9e57ea0cf455bab4958e3aa3b3b671b858', 'minion', 0, '2014-04-08 16:46:46', '2014-04-26 16:30:56'),
-(9, 'Peito', 'pepito@gmail.com', 'd3616137c6db93c288b8d127aa11b69272a9fe59', 'minion', 0, '2014-04-26 16:31:24', '2014-04-26 16:31:58'),
+(9, 'Pepito', 'pepito@gmail.com', '32e3f167be19a051debd2121d9e90ac9597f9375', 'minion', 1, '2014-04-26 16:31:24', '2014-04-27 18:15:25'),
 (10, 'Jhonny', 'lol@gmail.com', 'fa5dcc789967c146828a3c557306f3dee3a4bf48', 'minion', 0, '2014-04-26 16:55:17', '2014-04-26 16:55:17');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
