dc382f598bd35b772926390d4b9a241b8bcfa882
darkbox <rgtresd@gmail.com>
2014-04-29 20:01:21 +0200

    Se pueden añadir pagos, pero no actualiza el estado de factura

--- app/Controller/PaymentsController.php
+++ app/Controller/PaymentsController.php
@@ -24,6 +24,17 @@ class PaymentsController extends AppController {
 	public function index() {
 		$this->Payment->recursive = 0;
 		$this->set('payments', $this->Paginator->paginate());
+
+		$options = array(
+			'conditions' => array(
+				'status' => 1,
+				'status' => 2,
+				'status' => 3,
+				)
+			);
+
+		$invoices = $this->Payment->Invoice->find('list', $options);
+		$this->set(compact('invoices'));
 	}
 
 /**
@@ -35,39 +46,26 @@ class PaymentsController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Payment->create();
 			if ($this->Payment->save($this->request->data)) {
-				$this->Session->setFlash(__('The payment has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The payment could not be saved. Please, try again.'));
-			}
-		}
-		$invoices = $this->Payment->Invoice->find('list');
-		$this->set(compact('invoices'));
-	}
+				$this->Session->setFlash(__('The payment has been saved.'), 'flash_success');
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->Payment->exists($id)) {
-			throw new NotFoundException(__('Invalid payment'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->Payment->save($this->request->data)) {
-				$this->Session->setFlash(__('The payment has been saved.'));
+				// Actualizar factura aquí
+
+
 				return $this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The payment could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The payment could not be saved. Please, try again.'), 'flash_danger');
 			}
-		} else {
-			$options = array('conditions' => array('Payment.' . $this->Payment->primaryKey => $id));
-			$this->request->data = $this->Payment->find('first', $options);
 		}
-		$invoices = $this->Payment->Invoice->find('list');
+
+		$options = array(
+			'conditions' => array(
+				'status' => 1,
+				'status' => 2,
+				'status' => 3,
+				)
+			);
+
+		$invoices = $this->Payment->Invoice->find('list', $options);
 		$this->set(compact('invoices'));
 	}
 
@@ -85,9 +83,9 @@ class PaymentsController extends AppController {
 		}
 		$this->request->onlyAllow('post', 'delete');
 		if ($this->Payment->delete()) {
-			$this->Session->setFlash(__('The payment has been deleted.'));
+			$this->Session->setFlash(__('The payment has been deleted.'), 'flash_success');
 		} else {
-			$this->Session->setFlash(__('The payment could not be deleted. Please, try again.'));
+			$this->Session->setFlash(__('The payment could not be deleted. Please, try again.'), 'flash_danger');
 		}
 		return $this->redirect(array('action' => 'index'));
 	}}
