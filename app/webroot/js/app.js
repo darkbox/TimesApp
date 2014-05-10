40931119efe5d5f3f2df3ba78d0f0b64621d680d
souanyirer <souanyirer@gmail.com>
2014-05-10 16:04:37 +0200

    Corregida validación de email con abide

--- app/webroot/js/app.js
+++ app/webroot/js/app.js
@@ -4,7 +4,8 @@ $(document).foundation({
   abide : {
     patterns: {
       pass: /^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])|(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W])|(?=.*[a-z])(?=.*[0-9])(?=.*[\\W])|(?=.*[A-Z])(?=.*[0-9])(?=.*[\\W])).{6,}$/,
-      invoiceNumber: /^\d{1,9}$/
+      invoiceNumber: /^\d{1,9}$/,
+      email: /^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{1,63}$/
     },
     validators: {
     	dateValidate: function(el, required, parent) {
